00fd72e15b44edb7806faff9cc1c53b48f27a76b
Brady Beutler <fadestone@users.noreply.github.com>
2022-03-14 13:23:42 -0700

    colonial downs

--- www/index.php
+++ www/index.php
@@ -2,12 +2,11 @@
 <!DOCTYPE html>
 <html lang="en" class="setTo100">
 <head>
-    <title>$290,000 Truckload of Winning by Tachi Palace Casino</title>
+    <title>Colonial Downs</title>
     <?php require_once __DIR__ . '/../../colonial-downs-portal/vendor/engaged-nation/portal-core/GoogleTagManager/Resources/public/gtm.php'; ?>
     <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=0">
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <meta name="description" content="Play free games online to score bonus chances to win a fully-loaded, 2020 Toyota Tacoma and more every week in November during Tachi Palace Casino’s $290K Truckload of Winning promotion!" />
-    <meta name="google-site-verification" content="WmmyIwOo9iywRf-N8Kwq3irT04cMkp5Vrgb9w1URGRY" />
+    <meta name="description" content="" />
     <base href="//<?php echo $_SERVER['HTTP_HOST']; ?>"/>
     <link rel="canonical" href="//<?php echo $_SERVER['HTTP_HOST']; ?>">
     <link rel="stylesheet" href="https://ds94t24nq4vzb.cloudfront.net/fontawesome/5-12-0/css/all.css">
